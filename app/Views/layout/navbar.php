c07027e771651cea941d84bd01fd4e3bbd22c991
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-03-26 19:02:09 +0700

    navbar first edition

---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,12 @@
+<nav>
+    <div class="navbar">
+        <a href="#" class="logo">xxiv</a>
+        <ul>
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">Our Books</a></li>
+            <li><a href="#">Memories</a></li>
+            <li><a href="#">DUDU</a></li>
+            <li><a href="#">Game</a></li>
+        </ul>
+    </div>
+</nav>
