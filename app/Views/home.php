57f341078c4a1c61810fcb218b3192cbc5fbeee2
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-03-28 04:07:48 +0700

    home's content basic layout + rotating moon

--- app/Views/home.php
+++ app/Views/home.php
@@ -8,8 +8,29 @@
     <h3 id="sub-txt">"Meraih Masa Depan"</h3>
     <a href="#about" class="nos-btn">Let's Nostalgic!</a>
 </section>
-<div class="about" id="about">
-    <h2>About Us</h2>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi culpa sint illum cupiditate ab iure esse, iste ea sunt itaque dolor illo et perspiciatis obcaecati, ullam hic, maiores vel similique!</p>
-</div>
+<section class="home" id="about">
+    <div>
+        <h2>About Us</h2>
+        <p> Kami adalah Angkatan ke-24 di SMA Pangudi Luhur Van Lith Muntilan. Berawal dari Orientasi Asrama & Sekolah (OASE) pada 21 Mei 2017, menjadikan tanggal tersebut sebagai titik awal angkatan ini lahir. Hingga akhirnya kami terlahir kembali ke dunia pada 20 Mei 2017 untuk menyebarkan Api Van Lith. Selama kami berdinamika disana, kami dibiasakan untuk selalu merefleksikan diri yang akhirnya menghasilkan dua buah <span><a href="<?= base_url('/books'); ?>">buku refleksi angkatan.</a></span> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi culpa sint illum cupiditate ab iure esse, iste ea sunt itaque dolor illo et perspiciatis obcaecati, ullam hic, maiores vel similique!</p>
+    </div>
+</section>
+<section class="home" id="member">
+    <h2>Spot Your Name</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium a tempora tenetur nostrum. Iusto commodi earum ducimus quas at reprehenderit quam qui totam quasi. Libero saepe natus dolore! Mollitia, dolore?</p>
+</section>
+<section class="home" id="statistic">
+    <h2>180 Anggota 180 Anggota 180 Anggota</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium a tempora tenetur nostrum. Iusto commodi earum ducimus quas at reprehenderit quam qui totam quasi. Libero saepe natus dolore! Mollitia, dolore?</p>
+</section>
+<section class="home socials-section" id="socials">
+    <h2>Get to Know Us Better with</h2>
+    <div class="socials-link">
+        <!-- <a href="https://www.instagram.com/patlikurgen/" class="btn socials-details" target="_blank">
+            <img src="src/instagram.png" alt="instagram-link"></a>
+        <a href="https://open.spotify.com/user/qpnrwvinmsitd38nz2kt3ij0v" class="btn socials-details" target="_blank">
+            <img src="src/spotify.png" alt="spotify-link"></a>
+        <a href="https://www.youtube.com/channel/UCjI3BssTlpPSHqnE_8B_mgQ" class="btn socials-details" target="_blank">
+            <img src="src/youtube.png" alt="youtube-link"></a> -->
+    </div>
+</section>
 <?= $this->endSection(); ?>
