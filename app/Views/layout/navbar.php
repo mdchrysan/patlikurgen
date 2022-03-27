3465a1abcc2f9c95897517dc8f75c576a0608243
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-03-27 23:57:33 +0700

    completing nav links, update nav with bootstrap, and failed toggle button

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,12 +1,17 @@
-<nav>
-    <div class="navbar">
-        <a href="#" class="logo">xxiv</a>
+<nav class="navbar">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand">xxiv</a>
+        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup"> -->
         <ul>
-            <li><a href="#" class="active">Home</a></li>
-            <li><a href="#">Our Books</a></li>
-            <li><a href="#">Memories</a></li>
-            <li><a href="#">DUDU</a></li>
-            <li><a href="#">Game</a></li>
+            <li><a class="nav-link" href="<?= base_url('/'); ?>" class="active">Home</a></li>
+            <li><a class="nav-link" href="<?= base_url('/books'); ?>">Our Books</a></li>
+            <li><a class="nav-link" href="<?= base_url('/memories'); ?>">Memories</a></li>
+            <li><a class="nav-link" href="<?= base_url('/dudu'); ?>">DUDU</a></li>
+            <li><a class="nav-link" href="<?= base_url('/game'); ?>">Game</a></li>
         </ul>
+        <!-- </div> -->
     </div>
 </nav>
