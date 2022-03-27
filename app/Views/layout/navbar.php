98c99bb757eafd01ae00158f035121eba7ef38bc
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-03-28 02:02:23 +0700

    add active nav with ternary

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -6,11 +6,26 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup"> -->
         <ul>
-            <li><a class="nav-link" href="<?= base_url('/'); ?>" class="active">Home</a></li>
-            <li><a class="nav-link" href="<?= base_url('/books'); ?>">Our Books</a></li>
-            <li><a class="nav-link" href="<?= base_url('/memories'); ?>">Memories</a></li>
-            <li><a class="nav-link" href="<?= base_url('/dudu'); ?>">DUDU</a></li>
-            <li><a class="nav-link" href="<?= base_url('/game'); ?>">Game</a></li>
+            <li class="nav-item">
+                <a class="nav-link <?= uri_string() == '/' ? 'active' : 'inactive'; ?>" href="<?= base_url('/'); ?>" class="active">
+                    Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?= uri_string() == 'books' ? 'active' : 'inactive'; ?>" href="<?= base_url('/books'); ?>">
+                    Our Books</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?= uri_string() == 'memories' ? 'active' : 'inactive'; ?>" href="<?= base_url('/memories'); ?>">
+                    Memories</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?= uri_string() == 'dudu' ? 'active' : 'inactive'; ?>" href="<?= base_url('/dudu'); ?>">
+                    DUDU</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?= uri_string() == 'game' ? 'active' : 'inactive'; ?>" href="<?= base_url('/game'); ?>">
+                    Game</a>
+            </li>
         </ul>
         <!-- </div> -->
     </div>
