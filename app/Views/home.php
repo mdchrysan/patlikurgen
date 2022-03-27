1ca0bcdad9221d8420efcf543d0b98f2e7534723
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-03-28 02:31:48 +0700

    bouncing nostalgic button

--- app/Views/home.php
+++ app/Views/home.php
@@ -6,9 +6,9 @@
     <img src="src/moon.png" alt="moon-img" id="moon">
     <h2 id="main-txt">Patlikurgen</h2>
     <h3 id="sub-txt">"Meraih Masa Depan"</h3>
-    <a href="#" class="btn">Let's Nostalgic!</a>
+    <a href="#about" class="nos-btn">Let's Nostalgic!</a>
 </section>
-<div class="about">
+<div class="about" id="about">
     <h2>About Us</h2>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi culpa sint illum cupiditate ab iure esse, iste ea sunt itaque dolor illo et perspiciatis obcaecati, ullam hic, maiores vel similique!</p>
 </div>
