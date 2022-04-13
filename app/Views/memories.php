7504e339914016c4a847d911957737d87fcb34c3
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-04-14 04:53:07 +0700

    change display method on cinema section

--- app/Views/memories.php
+++ app/Views/memories.php
@@ -166,29 +166,42 @@
     </section>
     <section class="cinema-1" id="videos">
         <h2>Videos</h2>
-        <div class="videos">
-            <div class="video">
-                <iframe src="https://www.youtube.com/embed/fMWG6A9k_gQ?controls=0&rel=0&modestbranding=1" title="Film Angkatan" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div class="row">
+            <div class="col-lg-6 px-0">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/fMWG6A9k_gQ?controls=0&rel=0&modestbranding=1" title="Film Angkatan" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
             </div>
-            <div class="video">
-                <iframe src="https://www.youtube.com/embed/2uR70dThaEw?controls=0&rel=0&modestbranding=1" title="Goes To Aftermovie" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="col-lg-6 px-0">
+                <div class="embed-responsive embed-responsive-16by9">
+
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/2uR70dThaEw?controls=0&rel=0&modestbranding=1" title="Goes To Aftermovie" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
             </div>
         </div>
     </section>
     <section class="cinema-2" id="short-movies">
         <h2>Short Movies</h2>
-        <div class="videos">
-            <div class="video">
-                <iframe src="https://www.youtube.com/embed/cKd4T8vYKCM?controls=0&rel=0&modestbranding=1" title="Nekat" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div class="row">
+            <div class="col-lg-6 px-0">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/cKd4T8vYKCM?controls=0&rel=0&modestbranding=1" title="Nekat" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
             </div>
-            <div class="video">
-                <iframe src="https://www.youtube.com/embed/8eYMAhG7dZo?controls=0&rel=0&modestbranding=1" title="24:00" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="col-lg-6 px-0">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/8eYMAhG7dZo?controls=0&rel=0&modestbranding=1" title="24:00" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
             </div>
-            <div class="video">
-                <iframe src="https://www.youtube.com/embed/aPO8g4OC-pU?controls=0&rel=0&modestbranding=1" title="Takut" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="col-lg-6 px-0">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/aPO8g4OC-pU?controls=0&rel=0&modestbranding=1" title="Takut" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
             </div>
-            <div class="video">
-                <iframe src="https://www.youtube.com/embed/zrcRC3deUSE?controls=0&rel=0&modestbranding=1" title="Bangunkan Aku Saat Engkau Kembali" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="col-lg-6 px-0">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zrcRC3deUSE?controls=0&rel=0&modestbranding=1" title="Bangunkan Aku Saat Engkau Kembali" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
             </div>
         </div>
     </section>
