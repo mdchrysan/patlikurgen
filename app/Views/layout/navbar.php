e1e19fcfcdf2f7dc02cd15fc3aa030699323e538
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-04-16 23:48:52 +0700

    navbar toggle half fixed

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,32 +1,32 @@
-<nav class="navbar">
+<nav class="navbar navbar-expand-md">
     <div class="container-fluid">
         <a href="#" class="navbar-brand">xxiv</a>
-        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navToggler" aria-controls="navToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">Menu</span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup"> -->
-        <ul>
-            <li class="nav-item">
-                <a class="nav-link <?= uri_string() == '/' ? 'active' : 'inactive'; ?>" href="<?= base_url('/'); ?>" class="active">
-                    Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link <?= uri_string() == 'books' ? 'active' : 'inactive'; ?>" href="<?= base_url('/books'); ?>">
-                    Our Books</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link <?= uri_string() == 'memories' ? 'active' : 'inactive'; ?>" href="<?= base_url('/memories'); ?>">
-                    Memories</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link <?= uri_string() == 'dudu' ? 'active' : 'inactive'; ?>" href="<?= base_url('/dudu'); ?>">
-                    DUDU</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link <?= uri_string() == 'game' ? 'active' : 'inactive'; ?>" href="<?= base_url('/game'); ?>">
-                    Game</a>
-            </li>
-        </ul>
-        <!-- </div> -->
-    </div>
+        <div class="collapse navbar-collapse" id="navToggler">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link <?= uri_string() == '/' ? 'active' : 'inactive'; ?>" href="<?= base_url('/'); ?>" class="active">
+                        Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?= uri_string() == 'books' ? 'active' : 'inactive'; ?>" href="<?= base_url('/books'); ?>">
+                        Our Books</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?= uri_string() == 'memories' ? 'active' : 'inactive'; ?>" href="<?= base_url('/memories'); ?>">
+                        Memories</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?= uri_string() == 'dudu' ? 'active' : 'inactive'; ?>" href="<?= base_url('/dudu'); ?>">
+                        DUDU</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?= uri_string() == 'game' ? 'active' : 'inactive'; ?>" href="<?= base_url('/game'); ?>">
+                        Game</a>
+                </li>
+            </ul>
+            <!-- </div> -->
+        </div>
 </nav>
