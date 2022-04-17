5c97807a5f617bde23e1172632cdd9b2e3447c5b
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-04-17 23:59:23 +0700

    fix about and socials

--- app/Views/home.php
+++ app/Views/home.php
@@ -10,8 +10,13 @@
 </section>
 <section class="home" id="about">
     <div>
-        <h2>About Us</h2>
-        <p> Kami adalah Angkatan ke-24 di SMA Pangudi Luhur Van Lith Muntilan. Berawal dari Orientasi Asrama & Sekolah (OASE) pada 21 Mei 2017, menjadikan tanggal tersebut sebagai titik awal angkatan ini lahir. Hingga akhirnya kami terlahir kembali ke dunia pada 20 Mei 2017 untuk menyebarkan Api Van Lith. Selama kami berdinamika disana, kami dibiasakan untuk selalu merefleksikan diri yang akhirnya menghasilkan dua buah <span><a href="<?= base_url('/books'); ?>">buku refleksi angkatan.</a></span> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi culpa sint illum cupiditate ab iure esse, iste ea sunt itaque dolor illo et perspiciatis obcaecati, ullam hic, maiores vel similique!</p>
+        <div class="row">
+            <div class="col-lg-7">
+                <h2>About Us</h2>
+                <p> Kami adalah Angkatan ke-24 di SMA Pangudi Luhur Van Lith Muntilan. Berawal dari Orientasi Asrama & Sekolah (OASE) pada 21 Mei 2017, menjadikan tanggal tersebut sebagai titik awal angkatan ini lahir. Hingga akhirnya kami terlahir kembali ke dunia pada 20 Mei 2017 untuk menyebarkan Api Van Lith. Selama kami berdinamika disana, kami dibiasakan untuk selalu merefleksikan diri yang akhirnya menghasilkan dua buah <span><a href="<?= base_url('/books'); ?>">buku refleksi angkatan.</a></span> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi culpa sint illum cupiditate ab iure esse, iste ea sunt itaque dolor illo et perspiciatis obcaecati, ullam hic, maiores vel similique!</p>
+            </div>
+            <img class="col-lg-5" src="https://pbs.twimg.com/media/CH6HWI3UAAAstX8.jpg" alt="foto angkatan">
+        </div>
     </div>
 </section>
 <section class="home" id="member">
@@ -24,15 +29,16 @@
 </section>
 <section class="home socials-section" id="socials">
     <h2>Get to Know Us Better with</h2>
-    <div class="socials-link">
-        <a href="https://www.instagram.com/patlikurgen/" class="btn socials-details" target="_blank">
+    <!-- <div class="socials-link"> -->
+    <div class="row">
+        <a href="https://www.instagram.com/patlikurgen/" class="btn col-lg-3" target="_blank">
             <img src="src/instagram.png" alt="instagram-link"></a>
-        <!-- <a href="https://open.spotify.com/user/qpnrwvinmsitd38nz2kt3ij0v" class="btn socials-details" target="_blank">
+        <a href="https://open.spotify.com/user/qpnrwvinmsitd38nz2kt3ij0v" class="btn col-lg-3" target="_blank">
             <img src="src/spotify.png" alt="spotify-link"></a>
-        <a href="https://www.youtube.com/channel/UCjI3BssTlpPSHqnE_8B_mgQ" class="btn socials-details" target="_blank">
+        <a href="https://www.youtube.com/channel/UCjI3BssTlpPSHqnE_8B_mgQ" class="btn col-lg-3" target="_blank">
             <img src="src/youtube.png" alt="youtube-link"></a>
-        <a href="mailto:yearbook.patlikur@gmail.com" class="btn socials-details" target="_blank">
-            <img src="src/gmail.png" alt="gmail-link"></a> -->
+        <a href="mailto:yearbook.patlikur@gmail.com" class="btn col-lg-3" target="_blank">
+            <img src="src/gmail.png" alt="gmail-link"></a>
     </div>
 </section>
 <?= $this->endSection(); ?>
