d50e90ee8003d5f4460076abb1b1e4fa43211427
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2022-04-20 23:58:56 +0700

    try implementing jquery marquee

--- app/Views/layout/head.php
+++ app/Views/layout/head.php
@@ -10,6 +10,17 @@
     <link rel="stylesheet" href="/style.css">
     <title><?= $title; ?></title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery.marquee@1.6.0/jquery.marquee.min.js"></script>
+    <script>
+        $('.marquee').marquee({
+            duration: 5000,
+            gap: 10,
+            delayBeforeStart: 0,
+            direction: 'left',
+            duplicated: true
+        });
+    </script>
 </head>
 
 <body>
